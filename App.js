ba61d11775e3d7f0b7c243b908a4298dbcc897c1
Erakk <vilavuo@gmail.com>
2018-04-22 04:28:56 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,51 +1,51 @@
 import React, { Component } from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
 import './App.css';
 import InputField from './components/InputField';
 import InputMessage from './components/InputMessage';
 
-//import Field from './components/Field.js';
-//import Message from './components/Message.js';
 
-class App extends Component {
+export default class App extends Component {
 
   constructor(props){
     super(props);
 
-    this.addMessage = this.addMessage.bind(this);
-    this.removeMessage= this.removeMessage.bind(this);
+    this.state = {
+      messages: [
+        {id:0, text: "eka"}
+      ],
+      nextId: 1
+    };
+
   }
 
-  addMessage(messageText){
+  addMessage = (messageText) => {
     let messages = this.state.messages.slice();
     messages.push({id: this.state.nextId, text: messageText});
     this.setState({
       messages: messages,
-      nextId: ++this.state.nextId
+      nextId: ++this.state.nextId 
     })
   }
 
-  removeMessage(id){
+  removeMessage = (id) => {
     this.setState({
-      messages: this.state.messages.filter((message, index) => message.id !== id)
+      messages: this.state.messages.filter((message) => message.id !== id)
     })
   }
 
   render() {
     return (
       <div className="App-walls">
-
+          <div className="Message-Overflow">
           {
             this.state.messages.map((message) => {
               return <InputMessage message={message} key={message.id} id={message.id} removeMessage = {this.removeMessage}/>
             })
           }
-
+          </div>
         <InputField messageText="" addMessage={this.addMessage}/>
 
       </div>
     );
   }
 }
-
-export default App;
